abb8bcfaa9ce9a33c118a3dbb0c714fc82ea867c
DINH NHAM NGUYEN <dinhnhamnguyen@gmail.com>
2023-03-15 13:29:17 +0900

    Try to use docker command instead of github actions

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -4,6 +4,12 @@ echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 # cat /etc/os-release
-echo "The second arg: $2"
+echo -e "The second arg: $2\n"
+echo -e "The third arg: $3\n"
 
+echo -e "pwd:\n"
+pwd
+echo -e "ls -la\n"
+ls -la
+echo -e "List environment variables:\n"
 env
